cf12af52b3655fef2c49e08bfef74ab6e8565a9f
mrafay-dev <mrafay333@gmail.com>
2026-02-15 14:02:38 +0000

    GitHub cleanup

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-# Start Prometheus 
-echo "Starting Prometheus..."
 /opt/prometheus/prometheus \
     --config.file=/etc/prometheus/prometheus.yml \
     --storage.tsdb.path=/opt/prometheus/data \
     --web.listen-address=:9090 &
 
-# Start Grafana 
-echo "Starting Grafana..."
 /opt/grafana/bin/grafana-server \
     --homepath=/opt/grafana \
     --config=/opt/grafana/conf/defaults.ini \
