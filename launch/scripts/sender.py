6ba2f096294b019cf84f74f16792575bbd137f6c
mrafay-dev <mrafay333@gmail.com>
2026-03-01 17:15:05 +0000

    Improved sender and receiver work

--- launch/scripts/sender.py
+++ launch/scripts/sender.py
@@ -6,65 +6,79 @@ import argparse
 import sys
 
 def main(target_ip, buffer_size, duration_seconds, packet_size):
-  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-  try:
-      sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, buffer_size)
-      actual_buffer = sock.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
-      print(f"Socket send buffer set to: {actual_buffer:,} bytes")
-  except Exception as e:
-      print(f"Warning: Could not set buffer size: {e}")
+    try:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, buffer_size)
+        actual_buffer = sock.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
+        print(f"Socket send buffer set to: {actual_buffer:,} bytes")
+    except Exception as e:
+        print(f"Warning: Could not set buffer size: {e}")
 
-  addr = (target_ip, 9000)
+    addr = (target_ip, 9000)
 
-  start_time_ns = time.time_ns()
-  end_time_ns = start_time_ns + (duration_seconds * 10**9)
+    start_time_ns = time.time_ns()
+    end_time_ns = start_time_ns + (duration_seconds * 10**9)
 
-  seq_num = 1
+    seq_num = 1
 
 
-  #min packets size of 16 (just the header)
-                #bytes   (max for me was: 8 * 8188 = 65504 bytes. UDP MAX = 65507 payload)
-  payload = b'x' * (packet_size - 16)      #ASCII x value   (bytes)
+    #min packets size of 16 (just the header)
+                    #bytes   (max for me was: 8 * 8188 = 65504 bytes. UDP MAX = 65507 payload)
+    payload = b'x' * (packet_size - 16)      #ASCII x value   (bytes)
 
-  #print(time.time())
+    #print(time.time())
 
-  try:
-      while time.time_ns() < end_time_ns:
-          timestamp_ns = time.time_ns()
-          #header is now 12 bytes
-          header = struct.pack('!Q Q', seq_num, timestamp_ns)     #! means most-sig. val first. Q is 64bit int (8bytes). d is double 8 bytes
-          packet = header + payload
+    try:
+        while time.time_ns() < end_time_ns:
+            timestamp_ns = time.time_ns()
+            #header is now 12 bytes
+            header = struct.pack('!Q Q', seq_num, timestamp_ns)     #! means most-sig. val first. Q is 64bit int (8bytes). d is double 8 bytes
+            packet = header + payload
 
-          sock.sendto(packet, addr)
-          seq_num += 1
+            sock.sendto(packet, addr)
+            seq_num += 1
 
-              
-  except KeyboardInterrupt:
-      print("Sender Stopped")
+                
+    except KeyboardInterrupt:
+        print("Sender Stopped")
 
-  finally:
-      time.sleep(1)
-      for i in range(0,100):
-          sock.sendto(b'STOP', addr)
-      sock.close()
-      time_elapsed_ns = time.time_ns() - start_time_ns
-      pps = seq_num / (time_elapsed_ns * 10**-9)
-      mbps =  (packet_size * pps) / 1_000_000    #megabits per second 
+    finally:
+        time.sleep(1)
+        for i in range(0,100):
+            sock.sendto(b'STOP', addr)
+        sock.close()
+        time_elapsed_ns = time.time_ns() - start_time_ns
+        pps = seq_num / (time_elapsed_ns * 10**-9)
+        mbps =  (packet_size * pps) / 1_000_000    #megabits per second 
 
-      print(f"Packets send: {seq_num - 1}")
-      print(f"Time elapsed: {time_elapsed_ns * 10**-9 - 1}s")
-      print(f"PPS: {pps:.2f}")
-      print(f"MBPS: {mbps:.2f}")
-      print(f"Packet Size: {packet_size:.2f}")
+        print(f"Packets send: {seq_num - 1}")
+        print(f"Time elapsed: {time_elapsed_ns * 10**-9 - 1}s")
+        print(f"PPS: {pps:.2f}")
+        print(f"MBPS: {mbps:.2f}")
+        print(f"Packet Size: {packet_size:.2f}")
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description="UDP Sender")
-  parser.add_argument("--target-ip", default="0.0.0.0", help="Local network target IP address ex. 192.168.1.x")
-  parser.add_argument("--buffer", type=int, default=2_147_483_647, help="Sender OS buffer size in bytes")
-  parser.add_argument("--duration", type=int, default=60, help="How long to send the packets for in seconds")
-  parser.add_argument("--packet-size", type=int, default=1400, help="Packet size in bytes")
+    parser = argparse.ArgumentParser(description="UDP Sender")
+    parser.add_argument("--target-ip", default="127.0.0.1", help="Local network target IP address ex. 192.168.1.x")
+    parser.add_argument("--buffer", type=int, default=2_147_483_647, help="Sender OS buffer size in bytes")
+    parser.add_argument("--duration", type=int, default=60, help="How long to send the packets for in seconds")
+    parser.add_argument("--packet-size", type=int, default=1400, help="Packet size in bytes")
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  main(args.target_ip, args.buffer, args.duration, args.packet_size)
+    if args.packet_size < 16:
+        parser.error("--packet-size must be at least 16 bytes (header size is 16).")
+    if args.packet_size > 65504:
+        parser.error("--packet-size must be at most 65504 bytes.")
+    if args.duration < 0:
+        parser.error("--duration must be at least greater than 0 seconds.")
+    if args.buffer < 0:
+        parser.error("--buffer must be at least greater than 0 bytes.")
+    
+        
+
+
+    print(f"Sending to destination: {args.target_ip}")
+
+    main(args.target_ip, args.buffer, args.duration, args.packet_size)
